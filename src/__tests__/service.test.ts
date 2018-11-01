dbf119e30f521319b6a4e7fada0d5645c9a829db
keshavm <keshavm@KESHAVM-W10>
2018-11-01 15:26:27 +0530

    test case changes

--- src/__tests__/service.test.ts
+++ src/__tests__/service.test.ts
@@ -1,6 +1,27 @@
 import { CfcHttpService } from '../index';
-test('Test http service', () =>{
-  let cfcHttpService:CfcHttpService;
+
+jest.mock('../index');
+test('Test http service', () => {
+  let cfcHttpService: CfcHttpService;
   expect(cfcHttpService.checkServiceIsAlive('alive')).toBe('Service is alive');
   //expect(Greeter('Keshav first test')).toBe('Hello Keshav first test');
 });
+
+// describe('Test http service', () => {
+  
+//   let cfcHttpService:CfcHttpService;
+//   beforeAll(() => {
+    
+//   })
+
+//   beforeEach(() => {
+//     //do something
+//   })
+
+//   afterAll(() => {
+//     //do something
+//   })
+
+//   test(/*...*/)
+  
+// }
