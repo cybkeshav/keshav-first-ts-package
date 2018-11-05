4f68928e68a5c3f3640dcaaaa039a1583d99030c
keshavm <keshavm@KESHAVM-W10>
2018-11-05 19:52:03 +0530

    remove injector

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
 
-@Injectable()
 export class MyHttpService {
   public cfBaseUrl = '';
   public cfBaseIdentityUrl = '';
